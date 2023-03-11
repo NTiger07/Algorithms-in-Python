4828aa4f223fc5b5b3a3c8fc616542ad879e652f
NTiger07 <falaleru@gmail.com>
2023-03-11 23:15:03 +0100

    simple factorial

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
-print("Que miras bobo")
+# print("Que miras bobo")
+
+def factorialTrial(n):
+    factorial = 1
+    for i in range(1, n+1):
+        factorial *= i
+    return factorial
+        
+print(factorialTrial(5))
