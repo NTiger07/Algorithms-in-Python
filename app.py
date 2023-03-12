ef3feb0f59bbd944be3ae0f127503dbff3456732
NTiger07 <falaleru@gmail.com>
2023-03-12 23:25:46 +0100

    recursion almost

--- app.py
+++ app.py
@@ -3,3 +3,14 @@
 def permuteRecur(string, pocket=""):
     if len(string) == 0:
         print(pocket)
+    else:
+        for i in range(len(string)):
+            letter = string[i]
+            head = string[0:i]
+            tail = string[i+1:]
+            together = head + tail
+            permuteRecur(together, letter + pocket)
+            
+permuteRecur("ABCD", "")
+            
+            
