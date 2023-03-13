fb8306043cdcc7b77fe84ed4c6fcf366383f4ee3
NTiger07 <falaleru@gmail.com>
2023-03-13 23:44:58 +0100

    fix

--- test.py
+++ test.py
@@ -6,12 +6,22 @@ def permutations(str):
         str= list(str) # convert to list
         print(str) # print the string
         
-        indexOfLastValue = len(str) - 1 # index of last value
+        index = len(str) - 1 # initial value of index is last value
         
-        while len(str) >= 2 and str[indexOfLastValue - 1] > str[indexOfLastValue]:
+        while len(str) >= 2 and str[index - 1] > str[index]:
             # check if the length of the string is at least 2
             # AND if the second to the last value is greater then the last value
-            indexOfLastValue -= 1 # if so, then decrement the index by 1
+            index -= 1 # if so, then decrement the index by 1
+            
+        str[index:] = reversed(str[index:]) #reverse the remaining items
+        
+        if index >= 0:
+            q = index
+            while str[index - 1] > str[index]:
+                index += 1
+            temp = str[index - 1]
+            str[index - 1] = str[index]
+            str[index] = temp
         
         
         
