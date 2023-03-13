cd0f9f45e3d0f0f78b31eb704bccd4830a492ce3
NTiger07 <falaleru@gmail.com>
2023-03-13 15:13:35 +0100

    iterative permutation

--- app.py
+++ app.py
@@ -1,5 +1,28 @@
 # print("Que miras bobo")
+from math import factorial
 
+def permutations(str):
 
-
+    for permutation in range(factorial(len(str))):
+        print(" ".join(str))
+        indexOfLastValue = len(str) - 1 #index of last element
+        
+        while indexOfLastValue > 0 and str[indexOfLastValue - 1] > str[indexOfLastValue]:
+            indexOfLastValue -= 1
+        str[indexOfLastValue:] = reversed(str[indexOfLastValue:])
+        if indexOfLastValue > 0:
+            q = indexOfLastValue
+            while str[indexOfLastValue - 1] > str[q]:
+                q += 1
+            temp = str[indexOfLastValue - 1]
+            str[indexOfLastValue - 1] = str[q]
+            str[q] = temp
+        
+s = "abc"
+s = list(s)
+permutations(s)
+    
+        
+        
+# permutations("Fad")
             
