fc916c916cc5263d41935667d32bee318ef5d206
NTiger07 <falaleru@gmail.com>
2023-03-14 22:44:46 +0100

    fix

--- app.py
+++ app.py
@@ -1,19 +1,1 @@
 # print("Que miras bobo")
-def binarySearchRecur(array, start, end, target):
-    while start <= end:
-        
-        mid = start + end - 1// 2
-        
-        
-        if array[mid] < target:
-            start1 = mid + 1
-            return binarySearchRecur(array, start1, end, target)
-            
-        elif array[mid] > target:
-            end1 = mid - 1
-            return binarySearchRecur(array, start, end1, target)
-            
-        else:
-            return mid
-arr = [1, 2, 3, 4, 5]
-print(binarySearchRecur(arr, 0, len(arr)-1, 4))
